416e8166644e167534d9f3bde2d5795fdf99243f
Sproodigyprog <sproodigyprog@gmail.com>
2021-04-20 20:54:45 +0400

    Removed negative margin from the "features" section

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
   // Actions on the header *****************************************************
 
   $(window).scroll(function() {
-    // if ($(window).scrollTop() > 0) $('.header').addClass('header_fixed')
-    // else $('.header').removeClass('header_fixed')
+    if ($(window).scrollTop() > 0) $('.header').addClass('header_fixed')
+    else $('.header').removeClass('header_fixed')
   })
 
   // Replace header__info ******************************************************
